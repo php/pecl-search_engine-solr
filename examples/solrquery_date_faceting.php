e74f141ed1d1049f65c5f2edb5c3de85a57c3e7d
Omar Shaban <omars@php.net>
2014-02-26 21:27:34 +0200

    Added an example file for date facet functions

---
+++ examples/solrquery_date_faceting.php
@@ -0,0 +1,39 @@
+<?php
+
+include "bootstrap.php";
+
+$options = array
+(
+		'hostname' => SOLR_SERVER_HOSTNAME,
+		'login'    => SOLR_SERVER_USERNAME,
+		'password' => SOLR_SERVER_PASSWORD,
+		'port'     => SOLR_SERVER_PORT,
+);
+
+$client = new SolrClient($options);
+
+$query = new SolrQuery('*:*');
+
+$query->setFacet(true);
+
+$query->addFacetDateField('manufacturedate_dt');
+
+$query->setFacetDateStart('2006-02-13T00:00:00Z');
+
+$query->setFacetDateEnd('2012-02-13T00:00:00Z');
+
+$query->setFacetDateGap('+1YEAR');
+
+$query->setFacetDateHardEnd(1);
+
+$query->addFacetDateOther('before');
+
+$updateResponse = $client->query($query);
+
+$response_array = $updateResponse->getResponse();
+
+$facet_data = $response_array->facet_counts->facet_dates;
+
+print_r($facet_data);
+
+echo "\n";
