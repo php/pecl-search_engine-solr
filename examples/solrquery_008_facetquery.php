105e9e815ad099c71b7816caa852689923394046
Omar Shaban <omars@php.net>
2014-03-02 17:03:43 +0200

    Added example for FacetQuery

---
+++ examples/solrquery_008_facetquery.php
@@ -0,0 +1,25 @@
+<?php
+
+include "bootstrap.php";
+
+$options = array
+(
+		'hostname' => SOLR_SERVER_HOSTNAME,
+		'login'    => SOLR_SERVER_USERNAME,
+		'password' => SOLR_SERVER_PASSWORD,
+		'port'     => SOLR_SERVER_PORT,
+);
+
+$client = new SolrClient($options);
+
+$query = new SolrQuery('*:*');
+
+$query->setFacet(true);
+
+$query->addFacetQuery('price:[* TO 500]')->addFacetQuery('price:[500 TO *]');
+
+$query_response = $client->query($query);
+
+$response = $query_response->getResponse();
+
+print_r($response->facet_counts->facet_queries);
