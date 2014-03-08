7a1998568b935a8e446de22ee49408add1603c35
Omar Shaban <omars@php.net>
2014-03-08 20:37:26 +0200

    Added SolrClient::system() fetch server info example

---
+++ examples/solrclient_008_system.php
@@ -0,0 +1,21 @@
+<?php
+
+include "bootstrap.php";
+
+$options = array
+(
+    'hostname' => SOLR_SERVER_HOSTNAME,
+    'login'    => SOLR_SERVER_USERNAME,
+    'password' => SOLR_SERVER_PASSWORD,
+    'port'     => SOLR_SERVER_PORT,
+);
+
+$client = new SolrClient($options);
+
+$query_response = $client->system();
+
+$response = $query_response->getResponse();
+
+print_r($response);
+
+?>
