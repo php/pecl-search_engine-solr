7f8f04ff7eab0ce4e478ac93b8589d0253d671a4
Omar Shaban <omars@php.net>
2014-06-16 15:32:25 +0400

    remove php curl dependency for static builds

--- php_solr.c
+++ php_solr.c
@@ -900,7 +900,6 @@ static zend_function_entry solr_utils_methods[] = {
 
 /* {{{ solr_module_deps  */
 static zend_module_dep solr_module_deps[] = {
-    /*ZEND_MOD_REQUIRED(PHP_CURL_EXTENSION_NAME)*/
     ZEND_MOD_REQUIRED(PHP_LIBXML_EXTENSION_NAME)
     ZEND_MOD_REQUIRED(PHP_JSON_EXTENSION_NAME)
     { NULL, NULL, NULL }
