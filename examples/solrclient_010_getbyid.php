5b67a537fc8e5c347600174c630b23021600feeb
Omar Shaban <omars@php.net>
2015-09-09 04:12:48 +0200

    Real Time Get Examples

---
+++ examples/solrclient_010_getbyid.php
@@ -0,0 +1,16 @@
+<?php
+
+include "bootstrap.php";
+
+$options = array
+(
+    'hostname' => SOLR_SERVER_HOSTNAME,
+    'login'    => SOLR_SERVER_USERNAME,
+    'password' => SOLR_SERVER_PASSWORD,
+    'port'     => SOLR_SERVER_PORT,
+	'path'	   => SOLR_SERVER_PATH
+);
+
+$client = new SolrClient($options);
+$response = $client->getById('GB18030TEST');
+print_r($response->getResponse());
---
+++ examples/solrclient_011_getbyids.php
@@ -0,0 +1,19 @@
+<?php
+
+include "bootstrap.php";
+
+$options = array
+(
+    'hostname' => SOLR_SERVER_HOSTNAME,
+    'login'    => SOLR_SERVER_USERNAME,
+    'password' => SOLR_SERVER_PASSWORD,
+    'port'     => SOLR_SERVER_PORT,
+	'path'	   => SOLR_SERVER_PATH
+);
+
+$client = new SolrClient($options);
+$response = $client->getByIds(['GB18030TEST', '6H500F0']);
+
+$client = new SolrClient($options);
+$response = $client->getById('GB18030TEST');
+print_r($response->getResponse());
