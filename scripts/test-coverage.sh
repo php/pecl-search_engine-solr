e5220c16d429abfe75d42e0f999f826b58231e3b
Omar Shaban <omars@php.net>
2016-01-20 11:00:30 +0200

    coverage script using configure

--- scripts/test-coverage.sh
+++ scripts/test-coverage.sh
@@ -7,13 +7,10 @@ SCRIPT_DIR=$(dirname $0)
 
 make distclean &>/dev/null || true
 phpize &&
-CFLAGS="--coverage -fprofile-arcs -ftest-coverage" \
-CXXFLAGS="-ftest-coverage" \
-LDFLAGS="--coverage" \
-./configure 
+./configure --enable-coverage 
 make -j4 &&
-lcov --directory . --zerocounters &&
-lcov --directory . --capture --initial --no-external --output-file coverage.info 
+lcov --directory . --zerocounters  &&
+lcov --directory . --capture --initial --output-file coverage.info 
 
 USE_PHP=${TEST_PHP_EXECUTABLE:-$(which php)}
 
@@ -21,8 +18,8 @@ TEST_PHP_EXECUTABLE=${USE_PHP} \
 REPORT_EXIT_STATUS=0 \
 ${USE_PHP} \
   -n -d open_basedir= -d output_buffering=0 -d memory_limit=-1 \
-  run-tests.php -q -n -d extension_dir=modules -d extension=solr.so tests/
+  run-tests.php -q -n -d extension=json.so -d extension=$PWD/modules/solr.so tests
 
-lcov --remove coverage.info "/usr*" -o coverag.info # remove output for external libraries
+lcov --remove coverage.info "/usr/*" -o coverage.info
 lcov --no-checksum --directory . --capture --output-file coverage.info
 genhtml --highlight --legend --output-directory report coverage.info
