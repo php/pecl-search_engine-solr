27f196cdf7d61a6fb47e9f3b3b086f8d86c93040
omars44 <omar@atumm.org>
2023-11-17 05:47:31 +0200

    php-8 only use RETURN_THROWS()

--- src/php7/php_solr_client.c
+++ src/php7/php_solr_client.c
@@ -1565,7 +1565,14 @@ PHP_METHOD(SolrClient, optimize)
 	zend_bool success = 1;
 
 	if (zend_parse_parameters(ZEND_NUM_ARGS(), "|zbb", &maxSegmentsZval, &softCommit, &waitSearcher) == FAILURE) {
+#if PHP_MAJOR_VERSION == 8
 		RETURN_THROWS();
+#else
+		php_error_docref(NULL, E_WARNING, "Invalid parameter");
+
+		return;
+#endif
+
 	}
 
 	SOLR_PARSE_PARAM_INT(maxSegmentsZval, maxSegments, maxSegmentsLen, "maxSegments");
