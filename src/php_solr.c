df1e0b65a6e08b989bc1df0e7a3332e6041ec2c4
Birgir Haraldsson <biggi@stefna.is>
2025-10-12 14:43:51 +0200

    Remove dependency on libxml extension
    Fixes #72
    
    Because we don't rely on the libxml extension, we can safely remove
    its dependency.
    We still rely on the libxml library for all xml related things.

--- src/php_solr.c
+++ src/php_solr.c
@@ -1136,7 +1136,6 @@ static zend_function_entry solr_utils_methods[] = {
 
 /* {{{ solr_module_deps  */
 static zend_module_dep solr_module_deps[] = {
-    ZEND_MOD_REQUIRED(PHP_LIBXML_EXTENSION_NAME)
     ZEND_MOD_REQUIRED(PHP_JSON_EXTENSION_NAME)
     ZEND_MOD_END
 };
